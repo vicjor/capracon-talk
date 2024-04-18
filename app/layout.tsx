da45a031d5c8cb4c4a1af3b93e288df967bf6920
Victor Jørgensen <victor.jorgens@gmail.com>
2024-04-18 22:29:03 +0200

    Remove boilerplate from template

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "CapraCon 2024 Nachspiel"
 };
 
 export default function RootLayout({
